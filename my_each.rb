8e0cf1585472cfd72185b1bb86778ee7b5207a78
Karly Mareka <karly.mareka@gmail.com>
2018-08-29 22:20:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,7 @@
 array_numbers = [1,2,3,4]
 
 def my_each(array_numbers)
-  i = 0 
-  while i < array_numbers.length
-    yield(i)
-  end 
+ yield(array_numbers)
 end
 
 my_each(array_numbers) do 
@@ -12,3 +9,4 @@ my_each(array_numbers) do
   while i < array_numbers.length
     puts array_numbers[i]
     i += 1 
+  end 
